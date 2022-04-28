136eafcb511605906301c1333bceca08067f816d
Brad Caron <bacaron245@gmail.com>
2022-04-28 19:42:48 -0400

    reason: initial commit

---
+++ fod_to_peaks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+lmax2=`jq -r '.lmax2' config.json`
+lmax4=`jq -r '.lmax4' config.json`
+lmax6=`jq -r '.lmax6' config.json`
+lmax8=`jq -r '.lmax8' config.json`
+lmax10=`jq -r '.lmax10' config.json`
+lmax12=`jq -r '.lmax12' config.json`
+lmax14=`jq -r '.lmax14' config.json`
+lmax=`jq -r '.lmax' config.json`
+ncores=8
+
+fod=`eval "echo \$lmax${lmax}"`
+
+[ ! -f peaks.nii.gz ] && sh2peaks ${fod} ./peaks.nii.gz -force -nthreads ${ncores} -quiet
---
+++ run_commit.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import amico
+import commit
+from commit import trk2dictionary
+
+def run_commit(track_path,wm_path,peaks_path,dwi_path,bvals_path,bvecs_paths,dPar,dPerp,dIso,model):
+
+    # create dictionary
+    trk2dictionary.run(
+        filename_tractogram = track_path,
+        filename_peaks = peaks_path,
+        filename_mask = wm_path,
+        fiber_shift = 0.5,
+        peaks_use_affine = True
+    )
+
+    # generate scheme file
+    amico.util.fsl2scheme(bvals_path,bvecs_path,'DWI.scheme')
+
+    # load data
+    mit = commit.Evaluation('.','.')
+    mit.load_data(dwi_path,'DWI.scheme')
+
+    # set forward model
+    mit.set_model( model )
+    d_par       = dPar           # Parallel diffusivity [mm^2/s] 1.7E-3
+    d_perps_zep = dPerp        # Perpendicular diffusivity(s) [mm^2/s] [ 0.51E-3 ]
+    d_isos      = dIso # Isotropic diffusivity(s) [mm^2/s] [ 1.7E-3, 3.0E-3 ]
+    mit.model.set( d_par, d_perps_zep, d_isos )
+    mit.generate_kernels( regenerate=True )
+    mit.load_kernels()
+
+    # load sparse dictionary
+    mit.load_dictionary( 'COMMIT' )
+
+    # bulid linear operator
+    mit.set_threads()
+    mit.build_operator()
+
+    # fit the model
+    mit.fit(tol_fun=1e-3,max_iter=1000)
+
+    # save results
+    mit.save_results()
+
+def main():
+
+    # load config.json
+    with open('config.json','r') as config_f:
+        config = json.load(config_f)
+
+    # grab appropriate variables and paths
+    track_path = config['track']
+    wm_path = 'wm.nii.gz'
+    peaks_path = 'peaks.nii.gz'
+    dwi_path = config['dwi']
+    bvals_path = config['bvals']
+    bvecs_path = config['bvecs']
+    dPar = config['dPar']
+    dPerp = config['dPerp']
+    dIso = config['dIso']
+    model = config['model']
+
+    # run commit
+    run_commit(track_path,wm_path,peaks_path,dwi_path,bvals_path,bvecs_paths,dPar,dPerp,dIso,model)
