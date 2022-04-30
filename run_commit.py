06d993102840195d39f2c1f35cc4e886849eec3b
Brad Caron <bacaron245@gmail.com>
2022-04-30 14:43:09 -0400

    reason: expose more parameters

--- run_commit.py
+++ run_commit.py
@@ -6,9 +6,11 @@ import commit
 from commit import trk2dictionary
 import json
 
-commit.setup()
 
-def run_commit(track_path,wm_path,peaks_path,dwi_path,bvals_path,bvecs_path,dPar,dPerp,dIso,model,fiber_shift,peaks_use_affine):
+
+def run_commit(track_path,wm_path,peaks_path,dwi_path,bvals_path,bvecs_path,dPar,dPerp,dIso,model,fiber_shift,peaks_use_affine,b0_thr,b0_min_signal,max_iters,lmax,min_seg_len,min_fiber_len,max_fiber_len,vf_THR,flip_peaks,blur_spacing,blur_core_extent,blur_gauss_extent,blur_gauss_min):
+
+    commit.setup(lmax=lmax)
 
     trk2dictionary.run(
         filename_tractogram = track_path,
@@ -16,7 +18,16 @@ def run_commit(track_path,wm_path,peaks_path,dwi_path,bvals_path,bvecs_path,dPar
         filename_mask = wm_path,
         path_out = './COMMIT',
         fiber_shift = fiber_shift,
-        peaks_use_affine = peaks_use_affine
+        peaks_use_affine = peaks_use_affine,
+        min_seg_len = min_seg_len,
+        min_fiber_len = min_fiber_len,
+        max_fiber_len = max_fiber_len,
+        vf_THR = vf_THR,
+        flip_peaks = flip_peaks,
+        blur_spacing = blur_spacing,
+        blur_core_extent = blur_core_extent,
+        blur_gauss_extent = blur_gauss_extent,
+        blur_gauss_min = blur_gauss_min
     )
 
     # generate scheme file
@@ -24,7 +35,7 @@ def run_commit(track_path,wm_path,peaks_path,dwi_path,bvals_path,bvecs_path,dPar
 
     # load data
     mit = commit.Evaluation('.','.')
-    mit.load_data(dwi_path,'DWI.scheme')
+    mit.load_data(dwi_path,'DWI.scheme',b0_thr=b0_thr,b0_min_signal=b0_min_signal)
 
     # set forward model
     mit.set_model( model ) # StickZepplinBall
@@ -43,21 +54,21 @@ def run_commit(track_path,wm_path,peaks_path,dwi_path,bvals_path,bvecs_path,dPar
     mit.build_operator(build_dir='tmp')
 
     # fit the model
-    mit.fit(tol_fun=1e-3,max_iter=1000)
+    mit.fit(tol_fun=1e-3,max_iter=max_iters)
 
     # save results
-    mit.save_results()
+    mit.save_results(stat_coef=stat_coef)
 
 def main():
 
     # load config.json
     with open('config.json','r') as config_f:
         config = json.load(config_f)
-        
+
     # make commit dir
     if not os.path.isdir('COMMIT'):
         os.mkdir('COMMIT')
-        
+
     # make tmp dir for operator building
     if not os.path.isdir('tmp'):
         os.mkdir('tmp')
@@ -75,9 +86,23 @@ def main():
     model = config['model']
     fiber_shift = config['fiber_shift']
     peaks_use_affine = config['peaks_use_affine']
+    b0_thr = config['b0_threshold'] # default 0
+    b0_min_signal = config['b0_min_signal'] # default 0
+    max_iters = config['max_iters'] # default 1000
+    stat_coef = config['stat_coef'] # default 'sum'; options; sum, mean, median, min, max, all
+    lmax = config['lmax']
+    min_seg_len = config['min_seg_len'] # default 0.001
+    min_fiber_len = config['min_fiber_len'] # default 0
+    max_fiber_len = config['max_fiber_len'] # default 250
+    vf_THR = config['vf_THR'] # default 0.1
+    flip_peaks = config['flip_peaks'] # default false
+    blur_spacing = config['blur_spacing'] # default 0.25
+    blur_core_extent = config['blur_core_extent'] # default 0.0
+    blur_gauss_extent = config['blur_gauss_extent'] # default 0
+    blur_gauss_min = config['blur_gauss_min'] # default 0.1
 
     # run commit
-    run_commit(track_path,wm_path,peaks_path,dwi_path,bvals_path,bvecs_path,dPar,dPerp,dIso,model,fiber_shift,peaks_use_affine)
-    
+    run_commit(track_path,wm_path,peaks_path,dwi_path,bvals_path,bvecs_path,dPar,dPerp,dIso,model,fiber_shift,peaks_use_affine,b0_thr,b0_min_signal,max_iters,lmax,min_seg_len,min_fiber_len,max_fiber_len,vf_THR,flip_peaks,blur_spacing,blur_core_extent,blur_gauss_extent,blur_gauss_min)
+
 if __name__ == '__main__':
     main()
